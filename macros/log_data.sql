871290adfdca0de465a2aae3cf1903561ffe8135
monini98dlpg-lang <monini98dlpg@gmail.com>
2026-07-13 01:37:37 -0400

    Fix accounting + add sales and inventory extract macros

--- macros/log_data.sql
+++ macros/log_data.sql
@@ -34,7 +34,39 @@
   {% set query %}
     SELECT TO_JSON_STRING(t) as row_json
     FROM (
-      SELECT * FROM lakehouse-dev-472612.acctng_account_receivable_publish.customer_balance LIMIT 5000
+      SELECT * FROM lakehouse-dev-472612.acctng_account_receivable_publish.AR_Customer_Aging_vw LIMIT 5000
+    ) t
+  {% endset %}
+  {% set results = run_query(query) %}
+  {% if results %}
+    {% for row in results %}
+      {% do log(row[0], info=True) %}
+    {% endfor %}
+  {% endif %}
+  select 1 as done
+{% endmacro %}
+
+{% macro log_sales_data() %}
+  {% set query %}
+    SELECT TO_JSON_STRING(t) as row_json
+    FROM (
+      SELECT * FROM lakehouse-dev-472612.sales_order_publish.order_header_trn LIMIT 5000
+    ) t
+  {% endset %}
+  {% set results = run_query(query) %}
+  {% if results %}
+    {% for row in results %}
+      {% do log(row[0], info=True) %}
+    {% endfor %}
+  {% endif %}
+  select 1 as done
+{% endmacro %}
+
+{% macro log_inventory_data() %}
+  {% set query %}
+    SELECT TO_JSON_STRING(t) as row_json
+    FROM (
+      SELECT * FROM lakehouse-dev-472612.invnt_branch_item_master_stage.branch_item_master_snapshot LIMIT 5000
     ) t
   {% endset %}
   {% set results = run_query(query) %}
