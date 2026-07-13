01e18d1fe606d7dca1741397ae381b53d8d786c7
monini98dlpg-lang <monini98dlpg@gmail.com>
2026-07-13 01:35:46 -0400

    Add mass data extraction macros for HR, Customer, Accounting

--- macros/log_data.sql
+++ macros/log_data.sql
@@ -1,18 +1,47 @@
-{% macro log_query_results() %}
+{% macro log_hr_data() %}
   {% set query %}
     SELECT TO_JSON_STRING(t) as row_json
     FROM (
-      SELECT * FROM lakehouse-dev-472612.hr_employee_master_ingest.employee_master LIMIT 100
+      SELECT * FROM lakehouse-dev-472612.hr_employee_master_ingest.employee_master LIMIT 5000
     ) t
   {% endset %}
-  
   {% set results = run_query(query) %}
-  
   {% if results %}
     {% for row in results %}
       {% do log(row[0], info=True) %}
     {% endfor %}
   {% endif %}
-  
+  select 1 as done
+{% endmacro %}
+
+{% macro log_customer_data() %}
+  {% set query %}
+    SELECT TO_JSON_STRING(t) as row_json
+    FROM (
+      SELECT * FROM lakehouse-dev-472612.custmr_customer_master_publish.customer_account_dim LIMIT 5000
+    ) t
+  {% endset %}
+  {% set results = run_query(query) %}
+  {% if results %}
+    {% for row in results %}
+      {% do log(row[0], info=True) %}
+    {% endfor %}
+  {% endif %}
+  select 1 as done
+{% endmacro %}
+
+{% macro log_accounting_data() %}
+  {% set query %}
+    SELECT TO_JSON_STRING(t) as row_json
+    FROM (
+      SELECT * FROM lakehouse-dev-472612.acctng_account_receivable_publish.customer_balance LIMIT 5000
+    ) t
+  {% endset %}
+  {% set results = run_query(query) %}
+  {% if results %}
+    {% for row in results %}
+      {% do log(row[0], info=True) %}
+    {% endfor %}
+  {% endif %}
   select 1 as done
 {% endmacro %}
